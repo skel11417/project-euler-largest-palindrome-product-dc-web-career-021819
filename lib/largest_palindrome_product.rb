0f8e6d1dc3bc304481919dd3c406e42267524c79
Sean <shonkelli@gmail.com>
2019-05-23 10:56:59 -0400

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,26 @@
 # Implement your procedural solution here!
+def is_palindrome?(num)
+  num_str = num.to_s
+  i = 0
+  while i < (num_str.length / 2)
+    if num_str[i] != num_str[-(i+1)]
+      return false
+    end
+    i += 1
+  end
+  return true
+end
+
+def largest_palindrome_product
+  (9801..999801).reverse_each do |num|
+    if is_palindrome?(num)
+      (100..999).reverse_each do |smaller_num|
+        if num % smaller_num == 0
+          if (num/smaller_num).to_s.length == 3
+            return num
+          end
+        end
+      end
+    end
+  end
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,31 @@
 # Implement your object-oriented solution here!
+
+
+class LargestPalindromeProduct
+
+  def is_palindrome?(num)
+    num_str = num.to_s
+    i = 0
+    while i < (num_str.length / 2)
+      if num_str[i] != num_str[-(i+1)]
+        return false
+      end
+      i += 1
+    end
+    return true
+  end
+
+  def answer
+    (9801..999801).reverse_each do |num|
+      if is_palindrome?(num)
+        (100..999).reverse_each do |smaller_num|
+          if num % smaller_num == 0
+            if (num/smaller_num).to_s.length == 3
+              return num
+            end
+          end
+        end
+      end
+    end
+  end
+end
